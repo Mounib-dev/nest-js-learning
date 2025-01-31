000fea99267f1a02f03eb561785effa508fe7518
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-01-31 20:56:04 +0100

    Protecting tasks routes by authentication

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -8,16 +8,17 @@ import {
   Patch,
   Post,
   Query,
+  UseGuards,
 } from "@nestjs/common";
 import { TasksService } from "./tasks.service";
-import { TaskStatus } from "./tasks-status.enum";
 import { CreateTaskDto } from "./dto/create-task.dto";
 import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
-import { filter } from "rxjs";
 import { UpdateTaskStatusDto } from "./dto/update-task-status.dto";
 import { Task } from "./dto/task.entity";
+import { AuthGuard } from "@nestjs/passport";
 
 @Controller("/tasks")
+@UseGuards(AuthGuard())
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
