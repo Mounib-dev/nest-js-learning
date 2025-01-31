4cd591670291d52b65b46fd5e71d47af40ee56ee
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-01-31 21:20:00 +0100

    Adding interceptor and an exclude decorator to not send back user sensitive data when creating a task

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -2,6 +2,7 @@ import { IsNotEmpty, Length } from "class-validator";
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { TaskStatus } from "./tasks-status.enum";
 import { User } from "src/auth/user.entity";
+import { Exclude } from "class-transformer";
 
 @Entity()
 export class Task {
@@ -20,5 +21,6 @@ export class Task {
   status: TaskStatus;
 
   @ManyToOne((_type) => User, (user) => user.tasks, { eager: false })
+  @Exclude({ toPlainOnly: true })
   user: User;
 }
