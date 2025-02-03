5c53e83206ddfde7bb74ad725c2924380084c097
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-02-03 21:32:00 +0100

    Adding a logger on task controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -9,6 +9,7 @@ import {
   Post,
   Query,
   UseGuards,
+  Logger,
 } from "@nestjs/common";
 import { TasksService } from "./tasks.service";
 import { CreateTaskDto } from "./dto/create-task.dto";
@@ -18,10 +19,12 @@ import { Task } from "./task.entity";
 import { AuthGuard } from "@nestjs/passport";
 import { GetUser } from "src/auth/get-user.decorator";
 import { User } from "src/auth/user.entity";
+import { filter } from "rxjs";
 
 @Controller("/tasks")
 @UseGuards(AuthGuard())
 export class TasksController {
+  private logger = new Logger("TasksController");
   constructor(private tasksService: TasksService) {}
 
   @Get()
@@ -29,6 +32,9 @@ export class TasksController {
     @Query() filterDto: GetTasksFilterDto,
     @GetUser() user: User,
   ): Promise<Task[]> {
+    this.logger.verbose(
+      `User "${user.username}" retrieving all tasks. Filters: ${JSON.stringify(filterDto)}`,
+    );
     return this.tasksService.getTasks(filterDto, user);
   }
 
@@ -40,6 +46,9 @@ export class TasksController {
   @Post()
   @HttpCode(201)
   createTask(@Body() createTaskDto: CreateTaskDto, @GetUser() user: User) {
+    this.logger.verbose(
+      `User "${user.username}" is creating a new task. Data: ${JSON.stringify(createTaskDto)}`,
+    );
     return this.tasksService.createTask(createTaskDto, user);
   }
 
